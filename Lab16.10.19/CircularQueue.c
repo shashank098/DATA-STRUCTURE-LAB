8424379a778a8647446cab7b9ec9a9f1f8fc9dc4
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2019-09-16 15:11:53 +0530

    Update CircularQueue.c

--- Lab16.10.19/CircularQueue.c
+++ Lab16.10.19/CircularQueue.c
@@ -1,129 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define N 10
-#define t 1
-#define f 0
-int isempty(int free)
-{
-  if(free==-1)
-  return t;
-  else
-  return f;
+#define size 18
 
+void enqueue(int q[], int *r, int *f){
+  	if((*r-*f == size-1)  ||  *r == *f-1)
+    	printf("Queue is full!\n");
+  	else{
+        if(*r == -1 && *f == -1)
+            *r = *f = 0;
+        else
+    	    *r = (*r+1) % size;
+        printf("Value to enqueue:");
+    	scanf("%d", &q[*r]);
+  	}
 }
-int isfull(int fr,int re)
-{
-	if((fr==re+1)||(fr==0&&re==(N-1)))
-		return t ;
-	else
-		return f;
 
 
-
+void dequeue(int q[], int *r, int *f){
+  	if(*f == -1)
+    	printf("Queue is empty!\n");
+  	else{
+    	printf("Item deleted: %d\n", q[*f]);
+    	if(*f == *r)
+            *f = *r = -1;
+        else
+            *f = (*f+1) % size;
+  	}
 }
-void enqueue(int*x,int*fro,int*rea,int a[])
-{
-	if(isfull(*fro,*rea))
-		printf("circular queue is full");
-	else
-	{
-      if(*fro==-1)
-		  *fro=0;
-	  *rea=(*rea+1)%N;
-	  a[*rea]=*x;
-	}
 
 
-
-
+void display(int q[], int *r, int *f){
+  	int i;
+ 	if(*f == -1)
+    	printf("Queue is empty!\n");
+	else{
+		printf("----------------------\n");	
+ 		for(i = *f;; i++){
+            i %= size;
+   			printf("%d ", q[i]);
+            if(*r == i)
+                break;
+  		}
+  		printf("\n----------------------\n");
+	}	
 }
-void dequeue(int*fre,int*ree )
-{
-	if(isempty(*fre))
-		printf("circular queue is empty");
-	else
-	{ if(*fre==*ree)
-		{
-			*fre=-1;
-			*ree=-1;
-			
-			
-			
-		}
-	  else
-	    {
-
-		  *fre=(*fre+1)%N;
-
-	    }
 
 
-	}
-
+void main(){
+	int q[20], r = -1, f = -1;
+  	int a;
+  	while(1){
+		printf("\n\n----MENU----\n");
+    	printf("\n1.Enqueue \n2.Dequeue\n3.Display\n4.Exit\n");
+    	scanf("%d", & a);
+    	switch (a) {
+    	case 1:
+      		enqueue(q, &r, &f);
+      		break;
+   		case 2:
+      		dequeue(q, &r, &f);
+      		break;
+    	case 3:
+      		display(q, &r, &f);
+      		break;
+    	case 4:
+      		exit(0);
+      		break;
+    	default:
+      		printf("INVALID CHOICE\n");
+    	}
+  	}
 }
-void display(int*frn,int*ren,int z[])
-{
 
-	int i;
-	if(isempty(*frn))
-	{
-		printf("circular queue is empty");
-
-	}
-	else
-	{
-		printf("\n the elements are->");
-		for(i=*frn;i!=*ren;i=(i+1)%N)
-		{
-			printf("element %d is:%d ",z[i]);
-
-		}
-		printf("element %d is:%d ",z[i]);
-
-	}
-
-}
-void main()
-{
-	int ele,ch,cq[N],rear=-1,front=-1;
-
-
-	while(1)
-	{
-		printf("\n CIRCULAR QUEUE\n enter your choice for operation ");
-		printf("\n 1-Enter element ");
-		printf("\n 2-Delete element ");
-		printf("\n 3-Display elements ");
-		printf("\n 4-Exit ");
-		printf("\n choice :");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			case 1: printf("\n enter the element:");
-				scanf("%d",&ele);
-				 enqueue(&ele,&front,&rear,cq);
-				 break;
-			case 2: printf("\n The delete operation taking place");
-				dequeue(&front,&rear);
-				break;
-			case 3: display(&front,&rear,cq);
-				 break;
-			case 4: exit(0);
-
-
-
-		}
-
-
-
-
-
-	}
-
-
-
-	
-	
-	
-	
-}

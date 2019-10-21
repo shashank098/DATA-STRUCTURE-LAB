37a5a5892827688fe4f20ab1572a4712e7315122
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2019-10-21 15:34:49 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+  int data;
+  struct node *next;
+};
+typedef struct node* node;
+
+node getnode()
+{
+  node temp;
+  temp = (node)malloc(sizeof(struct node));
+  return temp;
+}
+
+node insert(node first , int item)
+{
+  node temp,curr;
+  curr = first;
+  temp = getnode();
+  temp->data = item;
+  temp->next = NULL;
+  if(first == NULL)
+  {
+    return temp;
+  }
+  else
+  {
+    while(curr->next != NULL)
+    {
+      curr = curr->next;
+    }
+    curr->next = temp;
+    return first;
+  }
+}
+
+node concatenate(node first1, node first2)
+{
+  node curr;
+  curr = first1;
+  while(curr->next != NULL)
+  {
+    curr = curr->next;
+  }
+  curr->next = first2;
+  return first1;
+}
+
+node sort(node first){
+	int swapped = -1;
+	node curr, last, temp;
+	if(first == NULL){
+		printf("Linked list is empty");
+		return first;
+	}
+	else{
+		
+		last = NULL;
+		while(swapped != 0){
+			swapped = 0;
+			curr = first;
+			while(curr->next != last){
+				if(curr->data > (curr->next)->data){
+					temp->data = curr->data; 
+					curr->data = (curr->next)->data;
+					(curr->next)->data = temp->data;
+					}
+				swapped = 1;
+				curr = curr->next;
+				}
+			last = curr;
+			}
+		
+		}
+		return first;
+	}
+
+void main()
+{
+  node first1 = NULL , first2 = NULL , curr = NULL;
+  int ch = 0 , item = 0;
+  while(1)
+  {
+    printf("\nEnter your choice :\n1.Insert elements in list1\n2.Insert elements in list2\n3.Concatenate two lists\n4.Sort the list\n5.Exit\n");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+      case 1 : printf("\nEnter element to be inserted in list1 : ");
+               scanf("%d",&item);
+               first1 = insert(first1 , item);
+               break;
+      case 2 : printf("\nEnter element to be inserted in list2 : ");
+               scanf("%d",&item);
+               first2 = insert(first2 , item);
+               break;
+      case 3 : first1 = concatenate(first1 , first2);
+               printf("\nThe elements in list after concatenation:\n");
+               curr = first1;
+               while(curr != NULL)
+               {
+                 printf("%d\t",curr->data);
+                 curr = curr->next;
+               }
+               break;
+      case 4 : first1 = sort(first1);
+               curr = first1;
+               printf("\nThe elements after sorting is\n");
+               while(curr != NULL)
+               {
+                 printf("%d\t",curr->data);
+                 curr = curr->next;
+               }
+               break;
+      case 5 : exit(0);
+      default : printf("\nInvalid choice");
+                break;
+    }
+  }
+}
+
+
+   
